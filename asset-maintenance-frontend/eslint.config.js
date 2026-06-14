e50dc2d61931c1b4a1db84bda901505d8636aa9b
Archit <architshelar6@gmail.com>
2026-06-14 18:42:17 +0530

    ci: relax frontend ESLint rules to warn and add GitLab CI configuration

--- asset-maintenance-frontend/eslint.config.js
+++ asset-maintenance-frontend/eslint.config.js
@@ -17,5 +17,11 @@ export default defineConfig([
       globals: globals.browser,
       parserOptions: { ecmaFeatures: { jsx: true } },
     },
+    rules: {
+      'no-unused-vars': 'warn',
+      'react-refresh/only-export-components': 'warn',
+      'react-hooks/immutability': 'off',
+      'react-hooks/set-state-in-effect': 'off',
+    }
   },
 ])
