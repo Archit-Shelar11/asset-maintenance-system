62b5ab89ddc8814cffd4a8243ffdb02a5d847178
Archit <archit.shelar@mendix.com>
2026-07-14 22:07:29 +0530

    changed backend url

--- asset-maintenance-frontend/src/api.js
+++ asset-maintenance-frontend/src/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const api = axios.create({
   baseURL: window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
     ? 'http://localhost:8080'
-    : 'https://asset-maintenance-system-ftim.onrender.com',
+    : 'https://asset-maintenance-system-backend.onrender.com ',
   timeout: 60000,
 });
 
